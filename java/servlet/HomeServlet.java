4ffb42396acf2178eef10b1c5152f0caed6cb75c
Aman kumar <140726117+aman-kumar-00@users.noreply.github.com>
2026-04-02 12:52:51 +0530

    Add files via upload

--- java/servlet/HomeServlet.java
+++ java/servlet/HomeServlet.java
@@ -9,15 +9,20 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import model.Wallpaper;
 
 @WebServlet("/Home")
 public class HomeServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	
+	
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+	
 
 		System.out.println("called");
 
